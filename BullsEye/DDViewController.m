a7529693047d817b9275d26b450cb4e1d043cda5
Dani Demas <mediasand@wl-217-62.wpa.claremont.edu>
2014-09-03 14:34:33 -0700

    Added all the labels, buttons, and slider and changed the “hit me” alert to display the position of the slider

--- BullsEye/DDViewController.m
+++ BullsEye/DDViewController.m
@@ -13,6 +13,9 @@
 @end
 
 @implementation DDViewController
+{
+    int _currentValue;
+}
 
 - (void)viewDidLoad
 {
@@ -28,14 +31,22 @@
 
 - (IBAction)showAlert
 {
+    NSString *message = [NSString stringWithFormat:
+      @"The value of the slider is: %d", _currentValue];
+    
     UIAlertView *alertView = [[UIAlertView alloc]
       initWithTitle:@"Hello World!"
-      message:@"This is my first app"
+      message:message
       delegate:nil
       cancelButtonTitle:@"Awesome"
       otherButtonTitles:nil];
     
     [alertView show];
+}
+
+- (IBAction)sliderMoved:(UISlider *)slider
+{
+    _currentValue = lroundf(slider.value);
 }
 
 @end
