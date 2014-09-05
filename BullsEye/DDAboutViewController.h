a138d409948f35b2a78c43cb69973d0e3210677c
Dani Demas <mediasand@wl-217-62.wpa.claremont.edu>
2014-09-04 20:46:02 -0700

    Added an information page

---
+++ BullsEye/DDAboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  DDAboutViewController.h
+//  BullsEye
+//
+//  Created by Dani Demas on 9/4/14.
+//  Copyright (c) 2014 Dani Demas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
